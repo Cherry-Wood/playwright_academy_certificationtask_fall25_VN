713b95d0a5660c443504e3f2081dd51e8d91bb7f
Cherry-Wood <CherryWood@gmx.com>
2025-12-12 19:13:46 +0100

    establish page objects for E2E test

---
+++ scr/pages/tegb/login_page.ts
@@ -0,0 +1,47 @@
+import { Locator, Page } from "@playwright/test";
+import { RegisterAccountPage } from "./register_account_page.ts";
+import { DashboardPage } from "./dashboard_page.ts";
+
+export class LoginPage {
+  readonly page: Page;
+  readonly url = process.env.TEGB_FRONTEND_URL as string;
+  readonly usernameImput: Locator;
+  readonly passwordImput: Locator;
+  readonly loginButton: Locator;
+  readonly registerButton: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.usernameImput = page.locator("input[data-testid$='username-input']");
+    this.passwordImput = page.locator("input[data-testid$='password-input']");
+    this.loginButton = page.locator("[type='submit']");
+    this.registerButton = page.locator(
+      "button[data-testid$='register-button']"
+    );
+  }
+
+  async open() {
+    await this.page.goto(this.url);
+    return this;
+  }
+
+  async fillUsername(username: string) {
+    await this.usernameImput.fill(username);
+    return this;
+  }
+
+  async fillPassword(password: string) {
+    await this.passwordImput.fill(password);
+    return this;
+  }
+
+  async clickLogin() {
+    await this.loginButton.click();
+    return new DashboardPage(this.page);
+  }
+
+  async clickRegister() {
+    await this.registerButton.click();
+    return new RegisterAccountPage(this.page);
+  }
+}
